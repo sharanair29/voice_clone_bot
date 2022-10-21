95bf60a9928fd129ec300a590e0623898665b8e4
Akshara Sadheesh <94154731+sharanair29@users.noreply.github.com>
2022-10-21 18:44:37 +0800

    Update a2.py

--- a2.py
+++ a2.py
@@ -5,8 +5,7 @@ now = datetime.datetime.now()
 
 # print(now.hour)
 # print(now.minute)
-pwk.sendwhatmsg("+60125250692","Hope this works NOW", now.hour , now.minute + 1)
-# pwk.sendwhatmsg("+60125250692","Hope this works", 1 , 4)
-# pwk.sendwhatmsg_instantly("+60125250692","Testing")
+pwk.sendwhatmsg("+xxxxx","Hope this works NOW", now.hour , now.minute + 1)
+
 
 
